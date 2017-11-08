cd440ac869d5d7214607dc60df6efcf3507dab4b
John Valle <dwanevalle.dv@gmail.com>
2017-11-08 20:25:03 +0800

    gi change nako ang UI. other than that wala na ata.

---
+++ driver.js
@@ -0,0 +1,3 @@
+$("#addButt").click(function () {
+  $("#buttGroup").append('<div class="form-group" style="display:block"><input type="id" class="form-control input-id " id="id" placeholder="Passengers"></div>');
+});
