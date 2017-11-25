072ffeb82f98a39931ab6196b6447c29026a9056
Ara Marie Mejorada <32028775+aramariemejorada@users.noreply.github.com>
2017-11-26 01:49:56 +0800

    changed admin UI

--- admin.php
+++ admin.php
@@ -6,45 +6,52 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
         <link rel="stylesheet" href="css\bootstrap.min.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="css\style.css" type="text/css" rel="stylesheet">
     </head>
     <body>
-        <?php include 'header.php' ?>
+        <?php include 'includes\header.php' ?>
         <div class="container">
-            <ul class="nav nav-tabs" style="margin-left: 10px">
-                <li class="active"><a data-toggle="tab" href="#search">Search</a></li>
-                <li><a data-toggle="tab" href="#input">Input</a></li>
-            </ul>
-            <div class="tab-content">
-                <div id="search" class="tab-pane fade in active">
-                    <div id="tabs" class="tab-content container" style="border-radius: 5px">
-                        <div id="view" class="tab-pane fade in active">
-                            <div class="container">
-                                <center>
-                                <form action="php/search.inc.php" method="POST">
-                                	<div class="input-group"  style="padding-top: 50px; max-width: 370px;">
-                                        <input type="text" class="form-control" name="toSearch" placeholder="Driver ID/License Plate/Vehicle Type">
-                                        <span class="input-group-btn">
-                                            <button  class="btn btn-default" name="submit" type="submit">Search</button>
-                                        </span>
-                                    </div>
-                                </form>
-                                    <div class="btn-group" style="margin-bottom: 50px;">
-                                        <?php include 'printButtons.php' ?>
-                                    </div>
-                                </center> 
-                            </div>
-                        </div>
+        <ul class="nav nav-tabs" style="margin-left: 10px">
+            <li class="active"><a data-toggle="tab" href="#tripTick">Trip Ticket</a></li>
+            <li><a data-toggle="tab" href="#travelReport">Official Travel Report</a></li>
+            <li><a data-toggle="tab" href="#fuelConsumption">Fuel Consumption Report</a></li>
+        </ul>
+        <div class="tab-content">
+            <div id="tripTick" class="tab-pane fade in active">
+                <div id="tabs" class="tab-content container" style="border-radius: 5px">
+                    <div id="view" class="tab-pane fade in active">
+                        <?php include 'includes\searchbar.php' ?>
+                        <?php include 'driverData.php' ?>
                     </div>
                 </div>
-                <div id="input" class="tab-pane fade">
-                    <?php include 'form.php' ?>     
-                </div>
-            </div> 
-        </div>
-        <?php include 'help.php' ?>
+            </div>
+            <div id="travelReport" class="tab-pane fade">
+                <div id="tabs" class="tab-content container" style="border-radius: 5px">
+                    <div id="view" class="tab-pane fade in active">
+                        <?php include 'includes\searchbar.php' ?>
+                        <span class="logout">
+                            <button type="submit" class="btn btn-primary" name="print">Print</button>
+                        </span>
+                        <?php include 'vehicleData.php' ?>
+                    </div>
+                </div>    
+            </div>
+            <div id="fuelConsumption" class="tab-pane fade">
+                <div id="tabs" class="tab-content container" style="border-radius: 5px">
+                    <div id="view" class="tab-pane fade in active">
+                        <?php include 'includes\searchbar.php' ?>
+                        <span class="logout">
+                            <button type="submit" class="btn btn-primary" name="print">Print</button>
+                        </span>
+                        <?php include 'typeVehicleData.php' ?>
+                    </div>
+                </div>       
+            </div>
+        </div> 
+    </div>
+        <?php include 'includes\help.php' ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="css\style.css" type="text/css" rel="stylesheet">
         <script src="admin.js"></script>
     </body>
 </html>
