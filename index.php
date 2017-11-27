0b541d2a20a073bbc09d205d3701f1b4ae2327d7
Ara Marie Mejorada <32028775+aramariemejorada@users.noreply.github.com>
2017-11-27 12:07:22 +0800

    edited signup modal, added radio button

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
         <link rel="stylesheet" href="css\bootstrap.min.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="css\home.css" type="text/css" rel="stylesheet">
     </head>
-
     <body>
-        <div class="container-fluid">
-            <div class="container">
+        <div class="container-fluid" id="home">
+            <div class="container" id="con">
                 <div class="row">
                     <div id="dict" class="col-sm-8">
                         <center>
@@ -20,7 +20,7 @@
                     </div>
                 <form action="php/login.inc.php" method="POST">
                     <div id="form" class="col-sm-4">
-                            <div id="username" class="form-group">
+                            <div id="username" class="form-group" id="login">
 								<label for="name">Employee ID</label>
 						        <input type="text" name="empid" class="form-control" id="name" placeholder="Enter username" required="required" />
 								<label for="name" style="margin-top: 10px">Password</label>
@@ -37,38 +37,63 @@
                 </div>
             </div>
         </div>
+        <!-- modal for sign-up -->
         <div id="signup" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-            <div class="modal-dialog modal-lg" role="document" style="max-width: 300px">
+            <div class="modal-dialog modal-lg" role="document" style="max-width: 680px">
                 <div class="modal-content">
-                    <div class="container-fluid signup">
+                    <div class="container-fluid signup" style="margin: 0; margin-bottom: 20px">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                             <h3 class="modal-title" style="font-weight: bold"><center>Sign up</center></h3>
                         </div>
-                        <form action = "php/signup.inc.php" method = "POST" style="max-width:250px">
-	                        <div class="form-group">
-					            <label for="name">First name</label>
-					            <input type="text" class="form-control" name = "firstName" placeholder="Enter first name" required="required" />
-					           	<label for="name" style="margin-top: 10px">Last name</label>
-					            <input type="text" class="form-control" name = "lastName" placeholder="Enter last name" required="required" />
-					           	<label for="name" style="margin-top: 10px">Employee ID</label>
-					            <input type="text" class="form-control" name = "empid" placeholder="Enter Employee ID" required="required" />
-					           	<label for="name" style="margin-top: 10px">Password</label>
-					            <input type="password" class="form-control" name = "password" placeholder="Enter password name" required="required" />
-					           	<label for="name" style="margin-top: 10px">Re-Enter Password</label>
-					            <input type="password" class="form-control" name = "passwordcon" placeholder="Re-enter password" required="required" />
-					        </div>
-                            <center>
-                                <div class="col-lg-12">
-                                    <button type="submit" name ="submit" class="btn btn-primary" id="btnContactUs">Sign Up</button>
+                        <div class="container-fluid" style="margin-top: 10px">
+                            <form class="form-inline" action = "php/signup.inc.php" method = "POST">
+                                <div class="form-group" style="padding: 2px; padding-bottom: 10px;">
+                                    <medium class="form-text" style="display: block; font-weight:bold">First Name</medium>
+                                    <input type="text" class="form-control" name = "firstName" placeholder="Enter first name" required="required" />
                                 </div>
-                            </center>
-                        </form>
+                                <span>
+                                    <div class="form-group" style="padding: 2px; padding-bottom: 10px;">
+                                        <medium class="form-text" style="display: block; font-weight:bold">Middle Name</medium>
+                                        <input  type="text" class="form-control" name = "middleName" placeholder="Enter middle name" required="required" />
+                                    </div>
+                                    <div class="form-group" style="padding: 2px; padding-bottom: 10px;">
+                                        <medium class="form-text" style="display: block; font-weight:bold">Last Name</medium>
+                                        <input type="text" class="form-control" name = "lastName" placeholder="Enter last name" required="required" />
+                                    </div>
+                                </span>
+                                <br>
+                                <div class="form-group" style="padding: 2px; padding-bottom: 10px;">
+                                    <medium class="form-text" style="display: block; font-weight:bold">Employee ID</medium>
+                                    <input type="text" class="form-control" name = "empid" placeholder="Enter Employee ID" required="required" />
+                                </div>
+                                <span>
+                                    <div class="form-group" style="padding: 2px; padding-bottom: 10px;">
+                                        <medium class="form-text" style="display: block; font-weight:bold">Password</medium>
+                                        <input type="text" class="form-control" name = "middleName" placeholder="Enter password" required="required" />
+                                    </div>
+                                    <div class="form-group" style="padding: 2px; padding-bottom: 10px;">
+                                        <medium class="form-text" style="display: block; font-weight:bold">Re-enter password</medium>
+                                        <input type="text" class="form-control" name = "middleName" placeholder="Re-enter password" required="required" />
+                                    </div>
+                                </span>
+                                <label class="radio-inline">
+                                    <input type="radio" name="admin">Admin
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="driver">Driver
+                                </label>
+                            </form>
+                        </div>
+                        <center>
+                            <div class="col-lg-12">
+                                <button type="submit" name ="submit" class="btn btn-primary" id="btnContactUs">Sign Up</button>
+                            </div>
+                        </center>
                     </div>
                 </div>
             </div>
         </div>
-        <link rel="stylesheet" href="css\home.css" type="text/css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
