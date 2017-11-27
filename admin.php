2afa55e004253edc1908d702ed095b98e150947e
Ara Marie Mejorada <32028775+aramariemejorada@users.noreply.github.com>
2017-11-27 12:32:20 +0800

    added vehicle tab, add vehicle button

--- admin.php
+++ admin.php
@@ -15,6 +15,7 @@
             <li class="active"><a data-toggle="tab" href="#tripTick">Trip Ticket</a></li>
             <li><a data-toggle="tab" href="#travelReport">Official Travel Report</a></li>
             <li><a data-toggle="tab" href="#fuelConsumption">Fuel Consumption Report</a></li>
+            <li><a data-toggle="tab" href="#vehicles">Vehicles</a></li>
         </ul>
         <div class="tab-content">
             <div id="tripTick" class="tab-pane fade in active">
@@ -44,6 +45,13 @@
                             <button type="submit" class="btn btn-primary" name="print">Print</button>
                         </span>
                         <?php include 'typeVehicleData.php' ?>
+                    </div>
+                </div>       
+            </div>
+            <div id="vehicles" class="tab-pane fade">
+                <div id="tabs" class="tab-content container" style="border-radius: 5px">
+                    <div id="view" class="tab-pane fade in active">
+                        <?php include 'includes\vehicles.php' ?>
                     </div>
                 </div>       
             </div>
---
+++ includes/vehicles.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <form>
+        <span class="logout">
+            <button type="add" class="btn btn-primary" name="addVehicle">Add Vehicle</button>
+        </span>
+    </form>
+    <div class="container table-responsive" style="padding-left: 0">
+        <table id="table" class="table table-hover"> <!-- for bordered table add table-bordered in class -->
+            <thead>
+                <tr>
+                    <th>Vehicle Type</th>
+                    <th>Plate No.</th>
+                    <th>No. of Cylinder</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>NISSAN</td>
+                    <td>GSR-541</td>
+                    <td>4</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
