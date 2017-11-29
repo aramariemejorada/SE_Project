a7814fe3cc84a386390ab5558d8525cf3eacdb41
Ara Marie Mejorada <32028775+aramariemejorada@users.noreply.github.com>
2017-11-30 06:56:26 +0800

    added driver tab for view and deletion of driver

---
+++ includes/users.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="container table-responsive" style="padding-left: 0">
+        <table id="table" class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Employee ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Leo Manansala</td>
+                    <td>12345</td>
+                    <td><button type="submit" name="edit">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
