1eab9496f2fe95341b89e5c76d79ce53e70d8f25
Ara Marie Mejorada <32028775+aramariemejorada@users.noreply.github.com>
2017-12-19 15:38:49 +0800

    add super admin page

---
+++ super-admin.php
@@ -0,0 +1,86 @@
+<html>
+    <head>
+        <title>Trip Ticket | Admin</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+        <link rel="stylesheet" href="css\bootstrap.min.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="css\style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <?php include 'includes\header.php' ?>
+        <div class="container" style="background-color: white; padding: 20px; border-radius: 5px">
+        <input type="checkbox" id="select-all" style="margin-left: 7px"> Select All | <a href=#>Approve</a> | <a href=#>Delete</a>
+            <div class="table-responsive">
+                <table id="myTable" class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Employee I.D.</th>
+                                <th>Role</th>
+                                <th>Date Registered</th>
+                            </tr>
+                        </thead>
+                        <tbody style="font-size: 12px">
+                            <tr>
+                                <td><input type="checkbox" style="margin: 0"></td>
+                                <td>Ara Marie Cruz Mejorada</td>
+                                <td>00556A</td>
+                                <td>Driver</td>
+                                <td>10-17-98</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" style="margin: 0"></td>
+                                <td>Ara Marie Cruz Mejorada</td>
+                                <td>00556A</td>
+                                <td>Driver</td>
+                                <td>10-17-98</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" style="margin: 0"></td>
+                                <td>Ara Marie Cruz Mejorada</td>
+                                <td>00556A</td>
+                                <td>Driver</td>
+                                <td>10-17-98</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" style="margin: 0"></td>
+                                <td>Ara Marie Cruz Mejorada</td>
+                                <td>00556A</td>
+                                <td>Driver</td>
+                                <td>10-17-98</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" style="margin: 0"></td>
+                                <td>Ara Marie Cruz Mejorada</td>
+                                <td>00556A</td>
+                                <td>Driver</td>
+                                <td>10-17-98</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" style="margin: 0"></td>
+                                <td>Ara Marie Cruz Mejorada</td>
+                                <td>00556A</td>
+                                <td>Driver</td>
+                                <td>10-17-98</td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" style="margin: 0"></td>
+                                <td>Ara Marie Cruz Mejorada</td>
+                                <td>00556A</td>
+                                <td>Driver</td>
+                                <td>10-17-98</td>
+                            </tr>
+                        </tbody>
+                    </table>
+            </div>
+        </div> 
+    </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="scripts/jquery.min.js"></script>
+         <script src="scripts/bootstrap.min.js"></script>
+        <script src="scripts/script.js"></script>
+    </body>
+</html>
